271632ea5f5acf29eb4cd3bb9f2b1c07730690d4
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-02-21 08:45:22 +0200

    added key commands

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from turtle import Screen
+from paddle import Paddle
 
 #screen details
 screen = Screen()
 screen.bgcolor('black')
 screen.setup(width = 800, height = 600)
 screen.title("Fun With POGO")
+
+paddle = Paddle()
+
+# paddle key commands
+screen.listen()
+screen.onkey(paddle.up, "Up")
+screen.onkey(paddle.down, "Down")
 
 
 
