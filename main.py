b2907d5a9d4c98b52f6443799a7861f9691e37c8
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-02-21 08:25:40 +0200

    created screen

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 from turtle import Screen
 
+#screen details
+screen = Screen()
+screen.bgcolor('black')
+screen.setup(width = 800, height = 600)
+screen.title("Fun With POGO")
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
