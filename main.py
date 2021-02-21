13d8946b19a2a51129066197b0255b6d321b8919
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-02-21 10:46:39 +0200

    added reset position logic

--- main.py
+++ main.py
@@ -36,10 +36,18 @@ while game_is_on:
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
-    #detect collision with right paddle
+    #detect collision with paddle
     if (ball.distance(r_paddle) < 50 and ball.xcor()> 320 or
         ball.distance(l_paddle) < 50 and ball.xcor()< -320):
         ball.bounce_x()
+
+    #detect right paddle misses:
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    #detect left paddle misses:
+    if ball.xcor() < -380:
+        ball.reset_position()
 
 
 
