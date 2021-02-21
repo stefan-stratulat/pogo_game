a0297a03a1ae577aba74a01fc8cd56c4aefb2cc9
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-02-21 10:50:06 +0200

    added point counting functions

--- scoreboard.py
+++ scoreboard.py
@@ -11,7 +11,19 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.l_score = 0
         self.r_score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
         self.goto(-100,200)
         self.write(self.l_score, align = 'center', font = FONT)
         self.goto(100,200)
         self.write(self.r_score, align = 'center', font = FONT)
+
+    def l_point(self):
+        self.l_score += 1
+        self.update_scoreboard()
+    
+    def r_point(self):
+        self.r_score += 1
+        self.update_scoreboard()
