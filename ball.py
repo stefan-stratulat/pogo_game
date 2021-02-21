abba2d4e264f85dfb59038f6422234b2363c9c02
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-02-21 09:33:33 +0200

    initial commit

---
+++ ball.py
@@ -0,0 +1,13 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape('circle')
+        self.color('white')
+        self.penup()
+
+    def move(self):
+        self.goto(280,280)
